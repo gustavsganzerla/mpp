61acd5845100d0c1f100f8580745328bbb66d74d
Gustavo Sganzerla <77517507+gustavsganzerla@users.noreply.github.com>
2023-10-31 16:11:39 -0300

    added test to protein len

--- protein_profiler/urls.py
+++ protein_profiler/urls.py
@@ -6,6 +6,8 @@ app_name = "protein_profiler"
 
 urlpatterns = [
     path('test_view/', views.test_view, name = "test_view"),
-    path('prot_char/', views.prot_char, name = "prot_char")
+    path('prot_char/', views.prot_char, name = "prot_char"),
+    path('submitted_prot_char/', views.submitted_prot_char, name = "submitted_prot_char")
+    
 ]
 
--- protein_profiler/views.py
+++ protein_profiler/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
 from django.http import HttpResponse
 from .forms import proteinForm
+
+from io import StringIO
+from Bio import SeqIO
+
 
 # Create your views here.
 def test_view(request):
@@ -10,17 +15,43 @@ def test_view(request):
 
 
 def prot_char(request):
-    #form = proteinForm()
 
     if request.method == "POST":
         form = proteinForm(request.POST, request.FILES)
+        output = []
         if form.is_valid():
             collected_data = form.cleaned_data
             protein_text = collected_data.get('protein_text')
             uploaded_file = collected_data.get('uploaded_file')
+
+            if uploaded_file:
+                uploaded_file_data = uploaded_file.read().decode('utf-8')
+                request.session['uploaded_file_data'] = uploaded_file_data
+
+                uploaded_file_io = StringIO(uploaded_file_data)
+
+                for record in SeqIO.parse(uploaded_file_io, "fasta"):
+                    protein = record.seq
+                    protein = str(protein)
+
+                    description = record.description
+
+                    output.append({
+                        'length':len(protein)
+                    })
+
+                    request.session['output'] = output
+                    return redirect(reverse('protein_profiler:submitted_prot_char'))
     else:
         form = proteinForm()
 
             
 
     return render(request, 'protein_profiler/prot_char.html', {'form': form})
+
+
+def submitted_prot_char(request):
+    output = request.session.get('output', None)
+
+    return render(request, "protein_profiler/submitted_prot_char.html",
+                  context={'output':output})
