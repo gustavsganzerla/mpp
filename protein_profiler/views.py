a90d70f60c507a5e368c9a02997f7b7f1ecc3430
Gustavo Sganzerla <77517507+gustavsganzerla@users.noreply.github.com>
2023-11-01 10:53:15 -0300

    added gravy function

--- protein_profiler/views.py
+++ protein_profiler/views.py
@@ -9,6 +9,38 @@ from Bio.SeqUtils.ProtParam import ProteinAnalysis
 from io import StringIO
 import re
 
+# Calculations
+def gravy(protein):
+    gravy_values = {
+        'A':  1.8 , 
+        'R': -4.5,  
+        'N': -3.5,  
+        'D': -3.5,  
+        'C':  2.5,  
+        'Q': -3.5,  
+        'E': -3.5,  
+        'G': -0.4,  
+        'H': -3.2,  
+        'I':  4.5,  
+        'L':  3.8,  
+        'K': -3.9,  
+        'M':  1.9,  
+        'F':  2.8,  
+        'P': -1.6,  
+        'S': -0.8,  
+        'T': -0.7,  
+        'W': -0.9,  
+        'Y': -1.3,  
+        'V':  4.2  
+        }
+
+    sequence_gravy = []
+    for aminoacid in protein:
+        gravy_value = gravy_values.get(aminoacid)
+        sequence_gravy.append(gravy_value)
+    return round(sum(sequence_gravy)/len(protein),2)
+
+
 # Create your views here.
 def test_view(request):
     return render(request, "protein_profiler/home.html")
@@ -59,7 +91,7 @@ def prot_char(request):
                     output.append({
                         'accession': accession,
                         'length': len(protein),
-                        #'gravy': gravy(protein),
+                        'gravy': gravy(protein)
                         #'aliphatic_index': aliphatic_index(protein),
                         #'instability_index': instability_index(protein),
                         #'stability':stability,
@@ -111,7 +143,7 @@ def prot_char(request):
                     output.append({
                         'accession': str(accession),
                         'length': len(protein),
-                        #'gravy': gravy(protein),
+                        'gravy': gravy(protein)
                         #'aliphatic_index': aliphatic_index(protein),
                         #'instability_index': instability_index(protein),
                         #'stability':stability,
