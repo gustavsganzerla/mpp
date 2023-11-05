67c22c3189360069a7456f7380e14f695fa8c14d
Gustavo Sganzerla <77517507+gustavsganzerla@users.noreply.github.com>
2023-11-05 00:44:24 -0300

    larger_text_Area

--- protein_profiler/forms.py
+++ protein_profiler/forms.py
@@ -2,7 +2,7 @@ from django import forms
 
 class proteinForm(forms.Form):
     protein_text = forms.CharField(widget=forms.Textarea(attrs={"rows":8, 
-                                                                "cols":30,
+                                                                "cols":40,
                                                                 "placeholder":">sp|A0A7H0DMZ8|IL18B_MONPV Interleukin-18-binding protein\n MRILFLIAFMYGCVHSYVNAVETKCPNLAIVTSSGEFHCSGCVERMPGFSYMYWLANDMKSDEDTKFIEHLGGGIKEDETVRTTDGGITTLRKVLHVTDTNKFAHYRFTCVLITLDGVSKKNIWLK"}), required=False)
     uploaded_file = forms.FileField(
         label = 'Upload your proteins in .FASTA format', required=False)
